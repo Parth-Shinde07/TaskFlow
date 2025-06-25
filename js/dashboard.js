859639fd199713883cb44c9439c9b97ed5eddab6
Parth-Shinde07 <parthshinde710@gmail.com>
2025-06-25 13:24:42 +0530

    made responsive

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,24 @@
 const ctx = document.getElementById('myChart');
-  new Chart(ctx, {
+new Chart(ctx, {
     type: 'pie',
     data: {
-      labels: ['Completed', 'In Progress', 'Pending'],
-      datasets: [{
-        data: [15, 5, 4],
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-      }]
+        labels: ['Completed', 'In Progress', 'Pending'],
+        datasets: [{
+            data: [15, 5, 4],
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                position: 'top', // or 'bottom', 'left', 'right'
+            },
+            title: {
+                display: true,
+                text: 'Task Status'
+            }
+        }
     }
-  });
-
+});
